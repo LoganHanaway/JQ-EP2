a845fbf3261d0ebe0f59d5e0a37e60fe7612882d
Logan Hanaway <lthanaway14@gmail.com>
2024-03-07 21:52:26 -0600

    finished except for sorting

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const $addMovie = $('#addMovie')
-const $filmList = $('#filmList')
+const $addMovie = $('#addMovie');
+const $filmList = $('#filmList');
+const $sort = $('#sort');
 
 
 const getValues = () => {
-    const title = $('#title').val()
-    const rating = $('#rating').val()
-    const $newMovie = `${title}: ${rating}/10 `
+    const title = $('#title').val();
+    const titleLength = $('#title').val().length;
+    const rating = $('#rating').val();
+    const $newMovie = `${title}: ${rating}/10 `;
+
+    if(titleLength < 2){
+        alert('Movie title must be longer than two characters');
+        return;
+    };
+
     $('<li></li>').text($newMovie).appendTo($filmList).append('<button id="remove" type="submit">Remove</button>').on('click', function(){
-        $(this).remove()
-    })
-}
+        $(this).remove();
+    });
+};
 
 $addMovie.on('click', function(e){
     e.preventDefault();
-    getValues()
+    getValues();
 })
 
+$sort.on('click', function(e){
+    e.preventDefault();;
+    sortOptions();
+});
+
+const sortOptions = () => {
+
+}
