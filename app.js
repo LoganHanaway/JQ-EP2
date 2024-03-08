140178eff788cb85806fed5ee6094f73e10eb04b
Logan Hanaway <lthanaway14@gmail.com>
2024-03-07 20:27:54 -0600

    setup done

---
+++ app.js
@@ -0,0 +1,14 @@
+const $title = $('#title').val()
+const $rating = $('#rating').val()
+const $addMovie = $('#addMovie')
+const $filmList = $('#filmList')
+
+function getValues(){
+    
+}
+
+
+$addMovie.on('click', function(){
+    $filmList
+})
+
