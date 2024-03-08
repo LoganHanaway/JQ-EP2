d32f5a94047a7d034037ddc265e3f4d843253002
Logan Hanaway <lthanaway14@gmail.com>
2024-03-07 21:21:58 -0600

    main done

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const $title = $('#title').val()
-const $rating = $('#rating').val()
 const $addMovie = $('#addMovie')
 const $filmList = $('#filmList')
 
-function getValues(){
-    
+
+const getValues = () => {
+    const title = $('#title').val()
+    const rating = $('#rating').val()
+    const $newMovie = `${title}: ${rating}/10 `
+    $('<li></li>').text($newMovie).appendTo($filmList).append('<button id="remove" type="submit">Remove</button>').on('click', function(){
+        $(this).remove()
+    })
 }
 
-
-$addMovie.on('click', function(){
-    $filmList
+$addMovie.on('click', function(e){
+    e.preventDefault();
+    getValues()
 })
 
